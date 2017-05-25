b8ab0198b6e244c4434b612439bfdd458d677e8a
Vit Herain <vit.herain@zonky.cz>
2017-05-25 21:59:38 +0200

    Section 5, Lecture 24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StocksComponent } from './stocks.component';
+import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StocksComponent,
+    MutualfundsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stocks.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'stocks',
+    template: '<h1>Stocks</h1>'
+})
+export class StocksComponent {
+
+}
