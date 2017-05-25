378001791c73c7380655863d435290cfaf81a88e
Vit Herain <vit.herain@zonky.cz>
2017-05-25 22:09:12 +0200

    Section 5, Lecture 25

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -2,8 +2,17 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'stocks',
-    template: '<h1>Stocks</h1>'
+    template: `<h1>Stocks</h1>
+    {{title}}
+    <ul>
+        <li *ngFor="let stock of stocks">
+            {{stock}}
+        </li>
+    </ul>
+    `
 })
 export class StocksComponent {
 
+    title = 'List of stocks';
+    stocks = ['APPLE', 'IBM', 'GOOGLE', 'UDEMY'];
 }
