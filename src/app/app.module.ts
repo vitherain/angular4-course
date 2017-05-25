c319a0a438d864dddda23d615abe82be5d3e1c8a
Vit Herain <vit.herain@zonky.cz>
2017-05-25 22:21:59 +0200

    Section 5, Lecture 27

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { StocksComponent } from './stocks.component';
 import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
+import { HighlightDirective } from './highlight.directive';
+import { StockDirectiveDirective } from './stock-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     StocksComponent,
-    MutualfundsComponent
+    MutualfundsComponent,
+    HighlightDirective,
+    StockDirectiveDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stock-directive.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StockDirectiveDirective } from './stock-directive.directive';
+
+describe('StockDirectiveDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StockDirectiveDirective();
+    expect(directive).toBeTruthy();
+  });
+});
