0f5c17d3b0d740da1dca5a4ad06681d4aca8a806
Vit Herain <vit.herain@zonky.cz>
2017-05-28 17:50:25 +0200

    Section 7, Lecture 48

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StocksComponent } from './stocks.component';
+import { DashboardComponent } from "app/dashboard/dashboard.component";
 
-const appRoutes: Routes = [{
-    path: 'stocks',
-    component: StocksComponent
-}];
+const appRoutes: Routes = [
+    {
+        path: 'stocks',
+        component: StocksComponent
+    },
+    {
+        path: '',
+        component: DashboardComponent
+    }
+];
 
 export const routing : ModuleWithProviders = RouterModule.forRoot(appRoutes);
