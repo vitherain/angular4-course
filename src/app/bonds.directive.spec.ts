56163f214ea2e5dfd235775fa98c8076a04584d1
Vit Herain <vit.herain@zonky.cz>
2017-05-28 15:46:12 +0200

    Section 5, Lecture 36

---
+++ src/app/bonds.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BondsDirective } from './bonds.directive';
+
+describe('BondsDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BondsDirective();
+    expect(directive).toBeTruthy();
+  });
+});
