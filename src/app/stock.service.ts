4159c14ad018889722e0a7460c044f0d3c41aa76
Vit Herain <vit.herain@zonky.cz>
2017-05-30 22:27:08 +0200

    Section 10, Lecture 61

--- src/app/stock.service.ts
+++ src/app/stock.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class StockService {
+
+    constructor (private http: Http) {
+
+    }
+
+    getStocksAPI(): Observable<any> {
+        return this.http.get('http://localhost:3000/stocks')
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().error || ' Server Error'));
+    }
 
     getStocks() : string[] {
         return ['APPLE', 'IBM', 'MICROSOFT'];
